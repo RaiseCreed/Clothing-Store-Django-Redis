cc5571e9fbaea28935ec3d578f16d7cb0eabe792
Dominik Nowacki <dominik.nowacki@bs4.io>
2024-08-07 23:55:12 +0200

    init files

---
+++ store/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# from django.views import View
+
+# # Create your views here.
+# class HomeView(View):
+
+# def homeView(request):
+#     return render(request,'home.html')
